6cd8a0e623583cca682b52618630df4a518601a3
cheikh kebe dia <cheikh-kebe.dia@usherbrooke.ca>
2020-01-31 01:44:27 -0500

    ProductionRound func

---
+++ helper/APIFunction/Production/ProductionRound.m
@@ -0,0 +1,16 @@
+function [games] = ProductionRound(token, uri, roundId)
+uri = uri + "/tournaments/rounds/?"+roundId;
+
+    header = matlab.net.http.HeaderField('X-token',token);
+    method = matlab.net.http.RequestMethod.GET;
+    body = [];
+
+    request = matlab.net.http.RequestMessage(method,header,body);
+
+    response = send(request,uri);
+  
+    response = response.Body.Data.games;
+    games = jsonencode(convertCharsToStrings(char(response)));
+    
+end
+
