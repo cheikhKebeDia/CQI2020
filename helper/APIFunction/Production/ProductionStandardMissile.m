ee9e69384b1a9e57592b3660e087b31ab317a339
cheikh kebe dia <cheikh-kebe.dia@usherbrooke.ca>
2020-01-31 04:23:10 -0500

    shooting function

---
+++ helper/APIFunction/Production/ProductionStandardMissile.m
@@ -0,0 +1,23 @@
+function [missileHit, gameEnd] = ProductionStandardMissile(token, uri, gamesId, column, row)
+    uri = uri + "/tournaments/rounds/games/"+gamesId;
+
+    contentTypeField = matlab.net.http.field.ContentTypeField('application/json');
+    data = struct("missileType", "standard", "parameters",struct("column", column, "row", row));
+    body = matlab.net.http.MessageBody(data);
+
+    type = matlab.net.http.MediaType('text/*');
+    acceptField = matlab.net.http.field.AcceptField([type]);
+    
+    xToken = matlab.net.http.field.GenericField("X-token",token);
+    
+    header = [contentTypeField acceptField xToken];
+    method = matlab.net.http.RequestMethod.POST;
+
+    request = matlab.net.http.RequestMessage(method,header,body);
+    response = send(request,uri);
+    
+    decodedResponse = jsondecode(convertCharsToStrings(char(response.Body.Data)));
+    missileHit = decodedResponse.missileHit;
+    gameEnd = decodedResponse.gameEnd;
+end
+
